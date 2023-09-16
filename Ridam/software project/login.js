402e51988a26a084a2f9fdfe1629f57f6500a378
Ridam Chhapiya <108975677+Ridam12@users.noreply.github.com>
2023-09-16 13:38:50 +0530

    Add files via upload

---
+++ Ridam/software project/login.js
@@ -0,0 +1,27 @@
+//for sign in animation
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+// for Password match test
+var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
